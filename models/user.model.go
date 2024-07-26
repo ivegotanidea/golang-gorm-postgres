06117cd42f498fb17136dccdbf1b7d26b84a48f8
orihomie <rblbl@mail.ru>
2024-07-26 19:13:48 +0300

    add nickname and json structs for user

--- models/user.model.go
+++ models/user.model.go
@@ -8,6 +8,7 @@ import (
 
 type User struct {
 	ID             uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	Name           string    `gorm:"type:varchar(20);not null"`
 	Phone          string    `gorm:"type:varchar(255)"`
 	TelegramUserId int64     `gorm:"type:bigint;not null"`
 	Password       string    `gorm:"type:varchar(42);not null"`
@@ -18,4 +19,36 @@ type User struct {
 	HasProfile     bool      `gorm:"type:boolean"`
 	Profiles       []Profile `gorm:"foreignKey:UserID"`
 	Services       []Service `gorm:"foreignKey:ClientUserID"`
+}
+
+type SignUpInput struct {
+	Phone    string `json:"phone" binding:"required"`
+	Password string `json:"password" binding:"required,min=8"`
+}
+
+type SignInInput struct {
+	Phone    string `json:"phone" binding:"required"`
+	Password string `json:"password" binding:"required,min=8"`
+}
+
+type BotSignUpInput struct {
+	Name           string `json:"name" binding:"required"`
+	Phone          string `json:"phone" binding:"required"`
+	TelegramUserId string `json:"email" binding:"required"`
+}
+
+type BotSignInInput struct {
+	TelegramUserId string `json:"email"  binding:"required"`
+}
+
+type UserResponse struct {
+	ID         uuid.UUID `json:"id"`
+	Name       string    `json:"name"`
+	Phone      string    `json:"phone"`
+	Avatar     string    `json:"photo,omitempty"`
+	Verified   bool      `json:"verified"`
+	HasProfile bool      `json:"has_profile"`
+	Provider   string    `json:"provider"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
 }
