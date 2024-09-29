6279466dcc86f163809acc7e5cd7d5639b8d0fba
orihomie <rblbl@mail.ru>
2024-09-29 11:05:24 +0300

    ability to hide ratings

--- models/service.model.go
+++ models/service.model.go
@@ -6,18 +6,20 @@ import (
 )
 
 type Service struct {
-	ID                 uuid.UUID      `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	ClientUserID       uuid.UUID      `gorm:"type:uuid;not null"`
-	ClientUserRatingID *uuid.UUID     `gorm:"type:uuid"`
-	ClientUserRating   *UserRating    `gorm:"foreignKey:ClientUserRatingID"`
-	ClientUserLat      string         `gorm:"type:varchar(10)"`
-	ClientUserLon      string         `gorm:"type:varchar(10)"`
-	ProfileID          uuid.UUID      `gorm:"type:uuid;not null"`
-	ProfileOwnerID     uuid.UUID      `gorm:"type:uuid;not null"`
-	ProfileRatingID    *uuid.UUID     `gorm:"type:uuid"`
-	ProfileRating      *ProfileRating `gorm:"foreignKey:ProfileRatingID"`
-	ProfileUserLat     string         `gorm:"type:varchar(10)"`
-	ProfileUserLon     string         `gorm:"type:varchar(10)"`
+	ID uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+
+	ClientUserID       uuid.UUID   `gorm:"type:uuid;not null"`
+	ClientUserRatingID *uuid.UUID  `gorm:"type:uuid"`
+	ClientUserRating   *UserRating `gorm:"foreignKey:ClientUserRatingID"` // profile owner's review on client
+	ClientUserLat      string      `gorm:"type:varchar(10)"`
+	ClientUserLon      string      `gorm:"type:varchar(10)"`
+
+	ProfileID       uuid.UUID      `gorm:"type:uuid;not null"`
+	ProfileOwnerID  uuid.UUID      `gorm:"type:uuid;not null"`
+	ProfileRatingID *uuid.UUID     `gorm:"type:uuid"`
+	ProfileRating   *ProfileRating `gorm:"foreignKey:ProfileRatingID"` // client's review on profile
+	ProfileUserLat  string         `gorm:"type:varchar(10)"`
+	ProfileUserLon  string         `gorm:"type:varchar(10)"`
 
 	DistanceBetweenUsers float64
 	TrustedDistance      bool
--- models/userRating.model.go
+++ models/userRating.model.go
@@ -9,6 +9,7 @@ type UserRating struct {
 	ID            uuid.UUID      `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
 	ServiceID     uuid.UUID      `gorm:"type:uuid;not null"`
 	UserID        uuid.UUID      `gorm:"type:uuid;not null"`
+	Hidden        bool           `gorm:"default:false"`
 	Review        string         `gorm:"type:varchar(2000)"`
 	Score         *int           `gorm:"type:int;not null"`
 	CreatedAt     time.Time      `gorm:"type:timestamp;not null"`
