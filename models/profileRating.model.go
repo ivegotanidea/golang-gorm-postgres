b722ddd1e2953154cbcf9c5a7389f0908ecea678
orihomie <rblbl@mail.ru>
2024-09-29 11:06:40 +0300

    rename property

--- models/profileRating.model.go
+++ models/profileRating.model.go
@@ -9,7 +9,7 @@ type ProfileRating struct {
 	ID               uuid.UUID         `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
 	ServiceID        uuid.UUID         `gorm:"type:uuid;not null"`
 	ProfileID        uuid.UUID         `gorm:"type:uuid;not null"`
-	Hidden           bool              `gorm:"default:false"`
+	ReviewTextHidden bool              `gorm:"default:false"`
 	Review           string            `gorm:"type:varchar(2000)"`
 	Score            *int              `gorm:"type:int;not null"`
 	CreatedAt        time.Time         `gorm:"type:timestamp;not null"`
--- models/userRating.model.go
+++ models/userRating.model.go
@@ -6,13 +6,13 @@ import (
 )
 
 type UserRating struct {
-	ID            uuid.UUID      `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	ServiceID     uuid.UUID      `gorm:"type:uuid;not null"`
-	UserID        uuid.UUID      `gorm:"type:uuid;not null"`
-	Hidden        bool           `gorm:"default:false"`
-	Review        string         `gorm:"type:varchar(2000)"`
-	Score         *int           `gorm:"type:int;not null"`
-	CreatedAt     time.Time      `gorm:"type:timestamp;not null"`
-	UpdatedAt     time.Time      `gorm:"type:timestamp;not null"`
-	RatedUserTags []RatedUserTag `gorm:"foreignKey:RatingID"`
+	ID               uuid.UUID      `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	ServiceID        uuid.UUID      `gorm:"type:uuid;not null"`
+	UserID           uuid.UUID      `gorm:"type:uuid;not null"`
+	ReviewTextHidden bool           `gorm:"default:false"`
+	Review           string         `gorm:"type:varchar(2000)"`
+	Score            *int           `gorm:"type:int;not null"`
+	CreatedAt        time.Time      `gorm:"type:timestamp;not null"`
+	UpdatedAt        time.Time      `gorm:"type:timestamp;not null"`
+	RatedUserTags    []RatedUserTag `gorm:"foreignKey:RatingID"`
 }
