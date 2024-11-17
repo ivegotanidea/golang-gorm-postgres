c7ba1de1f79128e9a1dba30dc8a02fcf437eee51
orihomie <rblbl@mail.ru>
2024-11-17 12:59:40 +0300

    setup logging for db queries

--- initializers/connectDB.go
+++ initializers/connectDB.go
@@ -2,7 +2,10 @@ package initializers
 
 import (
 	"fmt"
+	"gorm.io/gorm/logger"
 	"log"
+	"os"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -16,7 +19,19 @@ func ConnectDB(config *Config) {
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
 		config.DBHost, config.DBUserName, config.DBUserPassword, config.DBName, config.DBPort)
 
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// Create a custom logger
+	customLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // Output logs to the console
+		logger.Config{
+			SlowThreshold: time.Second, // Threshold for slow query logging
+			LogLevel:      logger.Info, // Log level: Info logs all queries
+			Colorful:      true,        // Enable color output
+		},
+	)
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: customLogger},
+	)
 	if err != nil {
 		log.Fatal("Failed to connect to the Database")
 	}
