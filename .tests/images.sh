be830769be34fc29556bc30fb601b8e84d97b69f
orihomie <rblbl@mail.ru>
2024-11-29 09:41:48 +0300

    upd img upload tests

--- .tests/images.sh
+++ .tests/images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-BASE_URL=http://localhost:80/api/v1
+BASE_URL=http://s2t5zmt3q3kibulra383.xyz/api/v1
 TELEGRAM_USER_ID=${1:-6794234746}
 PASSWORD=${2:-h5sh3d}
-PROFILE_ID=${3:-123e4567-e89b-12d3-a456-426614174000}
+PROFILE_ID=${3:-7e852ab6-878f-4f5b-84d2-2ee2749bfcee}
 
 IMAGES=(
     "/Users/orhanmamedov/Downloads/img_original.png"
@@ -12,10 +12,9 @@ IMAGES=(
 
 set -x
 
-access_token=$(curl -i -X POST ${BASE_URL}/auth/bot/login \
+access_token=$(curl -s -X POST ${BASE_URL}/auth/bot/login \
                     -H "Content-Type: application/json" \
-                    -d "{\"telegramUserId\": \"${TELEGRAM_USER_ID}\", \"password\": \"${PASSWORD}\"}" | \
-    grep "Set-Cookie" | grep "access_token" | cut -d '=' -f2 | cut -d ';' -f1)
+                    -d "{\"telegramUserId\": \"${TELEGRAM_USER_ID}\", \"password\": \"${PASSWORD}\"}" | jq -r .access_token)
 
 curl_cmd="curl -X POST ${BASE_URL}/images -F \"profileID=${PROFILE_ID}\" -b \"access_token=$access_token\""
 
